159767ff31274102ed39e830f9fdbf3a0e6def92
marmooo <marmooo@users.noreply.github.com>
2022-03-11 10:48:24 +0900

    improve help

--- src/drop-inline-css.js
+++ src/drop-inline-css.js
@@ -8,11 +8,12 @@ const parcelcss = require("@parcel/css");
 const htmlparser = require("node-html-parser");
 const { Command } = require("commander");
 
+const packageJson = JSON.parse(fs.readFileSync("package.json"));
 const program = new Command();
 program
-  .name("drop-inline-css")
-  .description("Parse HTML and drop unused CSS, inline it to HTML.")
-  .version("0.0.1");
+  .name(packageJson.name)
+  .description(packageJson.description)
+  .version(packageJson.version);
 program
   .argument("[input]", "Path of HTML file/direcotry")
   .option("-c, --css [path]", "CSS path for inlining in HTML")
