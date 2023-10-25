1f34169ab2a9a7fcbb9a963fc0209381f2644819
marmooo <marmooo@users.noreply.github.com>
2023-10-25 11:49:33 +0900

    bump node-html-parser from 6.1.5 to 6.1.10

--- mod.js
+++ mod.js
@@ -4,7 +4,7 @@ import { expandGlobSync } from "https://deno.land/std/fs/expand_glob.ts";
 // import dropcss from "npm:dropcss@1.0.16";
 import { PurgeCSS } from "npm:purgecss@5.0.0";
 import { transform } from "npm:lightningcss@1.21.0";
-import { parse } from "npm:node-html-parser@6.1.5";
+import { parse } from "npm:node-html-parser@6.1.10";
 
 async function getDroppedCss(css, html) {
   // const dropped = dropcss({
