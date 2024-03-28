4b7b83663fb54e765117a33c2505e4882a374d74
marmooo <marmooo@users.noreply.github.com>
2024-03-28 19:46:37 +0900

    0.3.1

--- build_npm.js
+++ build_npm.js
@@ -17,7 +17,7 @@ await build({
   },
   package: {
     name: "drop-inline-css",
-    version: "0.3.0",
+    version: "0.3.1",
     description: "Parse HTML and drop unused CSS, inline it to HTML.",
     license: "MIT",
     repository: {
