b4a31cb24b3086033e5a29c590c60d5dca502ecf
marmooo <marmooo@users.noreply.github.com>
2024-03-28 19:45:17 +0900

    bump lightningcss from 1.23.0 to 1.24.1

--- mod.js
+++ mod.js
@@ -3,7 +3,7 @@ import { basename, resolve } from "https://deno.land/std/path/mod.ts";
 import { expandGlobSync } from "https://deno.land/std/fs/expand_glob.ts";
 // import dropcss from "npm:dropcss@1.0.16";
 import { PurgeCSS } from "npm:purgecss@5.0.0";
-import { transform } from "npm:lightningcss@1.23.0";
+import { transform } from "npm:lightningcss@1.24.1";
 import { parse } from "npm:node-html-parser@6.1.12";
 
 async function getCss(url) {
